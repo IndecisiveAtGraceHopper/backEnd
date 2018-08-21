af338fb7ad5ebe93823d760f050464d12cead93c
harakirschman <kara.hirschman@gmail.com>
2018-08-21 15:15:20 -0400

    debug

--- server/api/index.js
+++ server/api/index.js
@@ -12,6 +12,7 @@ router.use('/', (req, res, next) => {
       res.header('Access-Control-Allow-Origin', origin)
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
       res.header('Access-Control-Allow-Headers')
+      next()
     } else {
       next(new Error('Not allowed by CORS'))
     }
