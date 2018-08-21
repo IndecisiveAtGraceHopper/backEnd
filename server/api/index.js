5f8be2d758e64503c77e19ac01851f2bf4734c41
harakirschman <kara.hirschman@gmail.com>
2018-08-21 15:19:39 -0400

    working on cors

--- server/api/index.js
+++ server/api/index.js
@@ -12,6 +12,9 @@ router.use('/', (req, res, next) => {
       res.header('Access-Control-Allow-Origin', origin)
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
       res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept')
+      if ('OPTIONS' === req.method) {
+        res.sendStatus(200)
+      }
       next()
     } else {
       next(new Error('Not allowed by CORS'))
