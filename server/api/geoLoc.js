f566126a8d0c1c1280d0981afa4deb4c20b70676
harakirschman <kara.hirschman@gmail.com>
2018-08-21 21:18:42 -0400

    geoloc working

--- server/api/geoLoc.js
+++ server/api/geoLoc.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
 
 module.exports = router
 
 router.post('/geocode', async (req, res, next) => {
     try {
-        const location = req.address.split().join('+')
+        const location = req.body.address.split(' ').join('+')
         const {data} = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.REACT_APP_GOOGLE_MAPS_KEY}`)
         const latitude = data.results[0].geometry.location.lat
         const longitude = data.results[0].geometry.location.lng
