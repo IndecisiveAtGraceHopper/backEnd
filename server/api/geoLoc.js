3fbf0ef22ced93d2f4ae12c6c9eeda7ecd1ff52b
Karalyn Faye <karalyn.ferrari@gmail.com>
2018-08-22 13:43:03 -0400

    changed react-app-google to just google

--- server/api/geoLoc.js
+++ server/api/geoLoc.js
@@ -7,10 +7,11 @@ module.exports = router
 router.post('/geocode', async (req, res, next) => {
     try {
         const location = req.body.address.split(' ').join('+')
-        const {data} = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.REACT_APP_GOOGLE_MAPS_KEY}`)
+
+        const {data} = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GOOGLE_MAPS_KEY}`)
         const latitude = data.results[0].geometry.location.lat
         const longitude = data.results[0].geometry.location.lng
-        res.status(200).send({latitude, longitude})        
+        res.status(200).send({latitude, longitude})
     } catch (err) {
         next(err)
     }
@@ -21,7 +22,7 @@ router.post('/address', async (req, res, next) => {
         const coords = req.body.coords
         const lat = coords[1]
         const lng = coords[0]
-        const addressObj = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.REACT_APP_GOOGLE_MAPS_KEY}`)
+        const addressObj = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.GOOGLE_MAPS_KEY}`)
         const address = addressObj.data.results[0].formatted_address
         res.status(200).send({address})
     } catch (err) {
